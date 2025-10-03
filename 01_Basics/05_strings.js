3a4488b6b88278b800557461b55eaa6509bbc032
Gaurav Singh Narwal <adityachaudhary.fs@gmail.com>
2025-10-03 11:41:02 +0000

    strings

--- 01_Basics/05_strings.js
+++ 01_Basics/05_strings.js
@@ -1,5 +1,38 @@
 const name = "gaurav"
-const repoCount = 60
+const repoCount = 50
 
-console.log(name + repoCount + " Value");
-console.log(name + " has " + repoCount + " repositories");
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+const gameName = new String('gaurav-gs')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf('r'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = "   gaurav   "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://gaurav.com/gaurav%20chaudhary"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('ram'))
+
+console.log(gameName.split('-'));
+
+
+
+
