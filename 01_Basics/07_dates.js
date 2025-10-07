ec2afb98eaf6f9cc423a3ad8ede2f7eac249fc86
Gaurav Singh Narwal <adityachaudhary.fs@gmail.com>
2025-10-07 16:09:15 +0000

    dates

--- 01_Basics/07_dates.js
+++ 01_Basics/07_dates.js
@@ -2,3 +2,9 @@
 
 let myDate = new Date()
 console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
+
+let myCreateDate = new Date(2018, 0, 23, 21, 30)
+console.log(myCreateDate.toLocaleString());
